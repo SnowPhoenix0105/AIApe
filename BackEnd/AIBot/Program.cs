69ec64c10db59644f8ef3a6d342ecce065b9406c
SnowPhoenix <1595763343@qq.com>
2021-04-21 13:55:23 +0800

    add Serilog

--- BackEnd/AIBot/Program.cs
+++ BackEnd/AIBot/Program.cs
@@ -6,11 +6,14 @@
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using Serilog;
 
 namespace Buaa.AIBot.BackEnd
 {
      public class Program
     {
+        public static readonly string LOG_FILE_PATH = "";
+
         public static void Main(string[] args)
         {
             CreateHostBuilder(args).Build().Run();
@@ -18,6 +21,14 @@ namespace Buaa.AIBot.BackEnd
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
             Host.CreateDefaultBuilder(args)
+                .UseSerilog((context, services, configureation) => 
+                {
+                    configureation
+                        .ReadFrom.Configuration(context.Configuration)
+                        .ReadFrom.Services(services)
+                        .WriteTo.Console()
+                        .WriteTo.File(LOG_FILE_PATH);
+                })
                 .ConfigureWebHostDefaults(webBuilder =>
                 {
                     webBuilder.ConfigureLogging((logging) =>
