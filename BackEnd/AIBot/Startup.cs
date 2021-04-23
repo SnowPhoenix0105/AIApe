3e422f592299bec7ad144221829a3e280b25ad9d
SnowPhoenix <1595763343@qq.com>
2021-04-23 21:08:32 +0800

    finish IP address record

--- BackEnd/AIBot/Startup.cs
+++ BackEnd/AIBot/Startup.cs
@@ -5,13 +5,15 @@
 
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
 using Microsoft.Bot.Builder;
 using Microsoft.Bot.Builder.Integration.AspNet.Core;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-
 using Buaa.AIBot.Bot;
+using Buaa.AIBot.Utils.Logging;
+using Serilog;
 
 namespace Buaa.AIBot
 {
@@ -34,6 +36,12 @@ namespace Buaa.AIBot
 
             // Create the bot as a transient. In this case the ASP Controller is expecting an IBot.
             services.AddTransient<IBot, EmptyBot>();
+
+            // Create HttpContextAccessor to access HttpContext at any time
+            services.AddTransient<IHttpContextAccessor, HttpContextAccessor>();
+
+            // Create IpAddressRecorder.
+            services.AddTransient<IpAddressRecorder>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -44,11 +52,23 @@ namespace Buaa.AIBot
                 app.UseDeveloperExceptionPage();
             }
 
-            app.UseDefaultFiles()
+            app
+                // Use serilog request-logging instead of origin.
+                .UseSerilogRequestLogging()
+
+                .UseDefaultFiles()
+
                 .UseStaticFiles()
+
                 .UseWebSockets()
+
                 .UseRouting()
+
                 .UseAuthorization()
+
+                // Record IP Address of the requester.
+                .UseIpAddressRecord()
+
                 .UseEndpoints(endpoints =>
                 {
                     endpoints.MapControllers();
--- BackEnd/AIBot/Utils/Logging/Extensions.cs
+++ BackEnd/AIBot/Utils/Logging/Extensions.cs
@@ -4,12 +4,16 @@ using System.Linq;
 using System.Threading.Tasks;
 using System.IO;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
 using Serilog;
 using Serilog.Events;
 using Serilog.Filters;
+using Serilog.Context;
 
 namespace Buaa.AIBot.Utils.Logging
 {
@@ -28,20 +32,24 @@ namespace Buaa.AIBot.Utils.Logging
                     var path = context.Configuration.GetSection("Path");
                     var logFileDir = Path.GetFullPath(path.GetValue<string>("LogFileDir"));
                     var logJsonDir = Path.GetFullPath(path.GetValue<string>("LogJsonDir"));
+                    var logIpDir = Path.GetFullPath(path.GetValue<string>("LogIpDir"));
                     Log.Information("Saving Log file in {LogFileDir}", logFileDir);
                     Log.Information("Saving Log json in {LogJsonDir}", logJsonDir);
+                    Log.Information("Saving IP Logging in {LogIpDir}", logIpDir);
                     configuration
                         .ReadFrom.Configuration(context.Configuration)
                         .ReadFrom.Services(services)
-                        // Enable LogContext.PushProperty()
+
+                        // Enable LogContext.PushProperty().
                         .Enrich.FromLogContext()
-                        // Set default logLevel (cover appsetting.json)
+
+                        // Set default logLevel (cover appsetting.json).
                         .MinimumLevel.Information()
 
-                        // Print log message to Console
+                        // Print log message to Console.
                         .WriteTo.Logger(subLogger =>
                         {
-                            // Logging exclude List
+                            // Logging exclude List.
                             var disableClasses = new HashSet<string>()
                             {
                                 "Microsoft.AspNetCore.Mvc.Infrastructure.ObjectResultExecutor",
@@ -49,17 +57,17 @@ namespace Buaa.AIBot.Utils.Logging
                                 "Microsoft.AspNetCore.Routing.EndpointMiddleware",
                             };
                             subLogger
-                                // The min level to print
+                                // The min level to print.
                                 .MinimumLevel.Information()
-                                // Exclude log-info from certain Classes
+                                // Exclude log-info from certain Classes.
                                 .Filter.ByExcluding(Matching.WithProperty<string>("SourceContext", t => disableClasses.Contains(t)))
                                 .WriteTo.Console();
                         })
 
-                        // Write log message to logfile
+                        // Write log message to logfile.
                         .WriteTo.Logger(subLogger =>
                         {
-                            // Logging exclude List
+                            // Logging exclude List.
                             var disableClasses = new HashSet<string>()
                             {
                                 "Microsoft.AspNetCore.Mvc.Infrastructure.ObjectResultExecutor",
@@ -67,51 +75,92 @@ namespace Buaa.AIBot.Utils.Logging
                                 "Microsoft.AspNetCore.Routing.EndpointMiddleware",
                             };
                             subLogger
+                                // The min level write to logfile.
+                                .MinimumLevel.Debug()
+                                // Exclude log-info from certain Classes.
+                                .Filter.ByExcluding(Matching.WithProperty<string>("SourceContext", t => disableClasses.Contains(t)))
+                                .WriteTo.File(
+                                    // Reading path of logfile from configuration.
+                                    path: Path.Combine(logFileDir, ".log"),
+                                    // allow buffering to reduce disk writting.
+                                    buffered: true,
+                                    outputTemplate: "{Timestamp:yyyy-MM-dd HH:mm:ss.fff} [{Level:u3}] [{RequestId}] {Message:lj}{NewLine}{Exception}",
+                                    // flush buffer each second.
+                                    flushToDiskInterval: TimeSpan.FromSeconds(1),
+                                    // create a new logfile each day.
+                                    rollingInterval: RollingInterval.Day,
+                                    // when logfile reach 4 MiB, create a new logfile.
+                                    fileSizeLimitBytes: 4 * 1024 * 1024,
+                                    rollOnFileSizeLimit: true
+                                    );
+                        })
+
+                        // Write IP to IP-logfile.
+                        .WriteTo.Logger(subLogger =>
+                        {
+                            subLogger
                                 // The min level write to logfile
                                 .MinimumLevel.Debug()
                                 // Exclude log-info from certain Classes
-                                .Filter.ByExcluding(Matching.WithProperty<string>("SourceContext", t => disableClasses.Contains(t)))
+                                .Filter.ByIncludingOnly(
+                                    Matching.WithProperty<string>("SourceContext", str => str == typeof(IpAddressRecorder).FullName)
+                                    )
                                 .WriteTo.File(
+                                    // Reading path of logfile from configuration.
+                                    path: Path.Combine(logIpDir, ".log"),
+                                    // allow buffering to reduce disk writting.
+                                    buffered: true,
+                                    outputTemplate: "{Timestamp:yyyy-MM-dd HH:mm:ss.fff} [{Level:u3}] [{RequestId}] [{RemoteIp}]{NewLine}",
+                                    // flush buffer each second.
+                                    flushToDiskInterval: TimeSpan.FromSeconds(10),
+                                    // create a new logfile each day.
+                                    rollingInterval: RollingInterval.Day,
+                                    // when logfile reach 4 MiB, create a new logfile
+                                    fileSizeLimitBytes: 4 * 1024 * 1024,
+                                    rollOnFileSizeLimit: true
+                                    );
+                        })
+
+                        // Write full log message to jsonfile.
+                        .WriteTo.Logger(subLogger =>
+                        {
+                            subLogger.
+                                MinimumLevel.Debug()
+                                .WriteTo.File(
+                                    formatter: new Serilog.Formatting.Compact.CompactJsonFormatter(),
                                     // Reading path of logfile from configuration
-                                    path: Path.Combine(logFileDir, ".log"),
+                                    path: Path.Combine(logJsonDir, ".json"),
                                     // allow buffering to reduce disk writting
                                     buffered: true,
-                                    outputTemplate: "{Timestamp:yyyy-MM-dd HH:mm:ss.fff} [{Level:u3}] [{RequestId}] {Message:lj}{NewLine}{Exception}",
-                                    // flush buffer each second
-                                    flushToDiskInterval: TimeSpan.FromSeconds(1),
+                                    // flush buffer every 10 second
+                                    flushToDiskInterval: TimeSpan.FromSeconds(10),
                                     // create a new logfile each day
                                     rollingInterval: RollingInterval.Day,
                                     // when logfile reach 4 MiB, create a new logfile
                                     fileSizeLimitBytes: 4 * 1024 * 1024,
                                     rollOnFileSizeLimit: true
-                                    )
-                                    ;
+                                );
                         })
 
-                        // Write full log message to jsonfile
-                        .WriteTo.File(
-                            formatter: new Serilog.Formatting.Compact.CompactJsonFormatter(),
-                            // Reading path of logfile from configuration
-                            path: Path.Combine(logFileDir, ".json"),
-                            // The min level write to logfile
-                            restrictedToMinimumLevel: LogEventLevel.Debug,
-                            // allow buffering to reduce disk writting
-                            buffered: true,
-                            // flush buffer every 10 second
-                            flushToDiskInterval: TimeSpan.FromSeconds(10),
-                            // create a new logfile each day
-                            rollingInterval: RollingInterval.Day,
-                            // when logfile reach 4 MiB, create a new logfile
-                            fileSizeLimitBytes: 4 * 1024 * 1024,
-                            rollOnFileSizeLimit: true
-                        )
-
-                        // Write full log message to debugger
-                        .WriteTo.Debug(
-                            restrictedToMinimumLevel: LogEventLevel.Debug
-                            )
+                        // Write full log message to debugger.
+                        .WriteTo.Debug(restrictedToMinimumLevel: LogEventLevel.Debug)
+                        
                         ;
                 });
+        }
+
+        public static IApplicationBuilder UseIpAddressRecord(this IApplicationBuilder app)
+        {
+            return app.Use(async (context, next) =>
+            {
+                var recorder = context.RequestServices.GetService<IpAddressRecorder>();
+                using (LogContext.PushProperty("RemoteIp", recorder.Remote))
+                // using (LogContext.PushProperty("LocalIp", recorder.Local))
+                {
+                    recorder.Logger.LogInformation("Remote IP = {RemoteIp}");
+                    await next.Invoke();
+                }
+            });
         }
     }
 }
