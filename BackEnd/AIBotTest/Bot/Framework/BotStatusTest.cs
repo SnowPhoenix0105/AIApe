2c86a42693904afd43084a7ae4244a778ab33fe1
SnowPhoenix <1595763343@qq.com>
2021-05-06 22:22:23 +0800

    add backend unit-test project

---
+++ BackEnd/AIBotTest/Bot/Framework/BotStatusTest.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using Xunit;
+using Buaa.AIBot.Bot.Framework;
+
+
+
+namespace AIBotTest.Bot.Framework
+{
+    public class BotStatusTest
+    {
+        [Theory]
+        [InlineData("")]
+        [InlineData("true")]
+        [InlineData("null")]
+        [InlineData("[abcd, efgh]")]
+        [InlineData("123456789")]
+        [InlineData("{\"msg\":true}")]
+        [InlineData("\"\"")]
+        public void GetSameAfterPut_String(string str)
+        {
+            IBotStatusContainer status = new BotStatus<int>();
+            status.Put("key", str);
+            var res = status.Get<string>("key");
+            Assert.Equal(str, res);
+        }
+    }
+}
