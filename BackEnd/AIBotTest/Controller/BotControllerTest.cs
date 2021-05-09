79a4a3d43a861e3e636081b2f0387f64b6d2ccba
李明昕 <18376161@buaa.edu.cn>
2021-05-09 16:39:58 +0800

    finish controller unit test

--- BackEnd/AIBotTest/Controller/BotControllerTest.cs
+++ BackEnd/AIBotTest/Controller/BotControllerTest.cs
@@ -1,7 +1,116 @@
+using System.Threading.Tasks;
+using System.Collections.Generic;
+using System;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Http;
 using Xunit;
 using Moq;
 
+using Buaa.AIBot.Services;
+using Buaa.AIBot.Services.Exceptions;
+using Buaa.AIBot.Services.Models;
+using Buaa.AIBot.Controllers;
+using Buaa.AIBot.Controllers.Models;
+using Buaa.AIBot.Repository;
+using Buaa.AIBot.Repository.Models;
+using Buaa.AIBot.Bot.Framework;
+using Microsoft.Extensions.Options;
+
 namespace AIBotTest.Controller
 {
-    
+    public class BotControllerTest
+    {
+        private Mock<IBotRunner> mockBot;
+
+        private Mock<IUserService> mockUser;
+
+        private BotBody body;
+
+        public BotControllerTest()
+        {
+            mockBot = new Mock<IBotRunner>();
+            mockUser = new Mock<IUserService>();
+            body = new BotBody();
+        }
+
+        [Fact]
+        public async Task StartAsyncTest()
+        {
+            mockUser.Setup(user => user.GetUidFromToken(It.IsAny<HttpRequest>()))
+            .Returns(0);
+            mockBot.Setup(bot => bot.Start(0)).ReturnsAsync
+            (
+                new OutputInfo
+                {
+                    Messages = new string[]
+                    {
+                        "Hello",
+                        "World"
+                    },
+                    Prompt = new string[]
+                    {
+                        "choice1",
+                        "choice2"
+                    }
+                }
+            );
+            BotController controller = new BotController(mockBot.Object, mockUser.Object);
+
+            var ret = await controller.StartAsync(body);
+            var okRet = Assert.IsType<OkObjectResult>(ret);
+            var resRet = okRet.Value;
+            var message = (IEnumerable<string>)resRet.GetType().GetProperty("Messages").GetValue(resRet, null);
+            var prompt = (IEnumerable<string>)resRet.GetType().GetProperty("Prompt").GetValue(resRet, null);
+            var messageIter = message.GetEnumerator();
+            messageIter.MoveNext();
+            Assert.Equal("Hello", messageIter.Current);
+            messageIter.MoveNext();
+            Assert.Equal("World", messageIter.Current);
+            var promptIter = prompt.GetEnumerator();
+            promptIter.MoveNext();
+            Assert.Equal("choice1", promptIter.Current);
+            promptIter.MoveNext();
+            Assert.Equal("choice2", promptIter.Current);
+        }
+
+        [Fact]
+        public async Task MessageAsyncTest()
+        {
+            mockUser.Setup(user => user.GetUidFromToken(It.IsAny<HttpRequest>()))
+            .Returns(0);
+            mockBot.Setup(bot => bot.Run(0, It.IsAny<InputInfo>())).ReturnsAsync
+            (
+                new OutputInfo
+                {
+                    Messages = new string[]
+                    {
+                        "Hello",
+                        "World"
+                    },
+                    Prompt = new string[]
+                    {
+                        "choice1",
+                        "choice2"
+                    }
+                }
+            );
+            BotController controller = new BotController(mockBot.Object, mockUser.Object);
+
+            var ret = await controller.MessageAsync(body);
+            var okRet = Assert.IsType<OkObjectResult>(ret);
+            var resRet = okRet.Value;
+            var message = (IEnumerable<string>)resRet.GetType().GetProperty("Messages").GetValue(resRet, null);
+            var prompt = (IEnumerable<string>)resRet.GetType().GetProperty("Prompt").GetValue(resRet, null);
+            var messageIter = message.GetEnumerator();
+            messageIter.MoveNext();
+            Assert.Equal("Hello", messageIter.Current);
+            messageIter.MoveNext();
+            Assert.Equal("World", messageIter.Current);
+            var promptIter = prompt.GetEnumerator();
+            promptIter.MoveNext();
+            Assert.Equal("choice1", promptIter.Current);
+            promptIter.MoveNext();
+            Assert.Equal("choice2", promptIter.Current);
+        }
+    }
 }
--- BackEnd/AIBotTest/Controller/UserControllerTest.cs
+++ BackEnd/AIBotTest/Controller/UserControllerTest.cs
@@ -1,6 +1,8 @@
 using System.Threading.Tasks;
+using System.Collections.Generic;
 using System;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Http;
 using Xunit;
 using Moq;
 
@@ -11,6 +13,8 @@ using Buaa.AIBot.Repository;
 using Buaa.AIBot.Repository.Models;
 using Buaa.AIBot.Repository.Exceptions;
 using Microsoft.Extensions.Options;
+
+using BNBCrypt = BCrypt.Net.BCrypt;
 
 namespace AIBotTest.Controller
 {
@@ -136,6 +140,343 @@ namespace AIBotTest.Controller
 
 
         [Fact]
+        public async Task GetInternalInfoAsync_Success()
+        {
+            mockServ.Setup(serv => serv.GetUidFromParameters(It.IsAny<HttpRequest>()))
+            .Returns(0);
+            mockRepo.Setup(repo => repo.SelectUserByIdAsync(0)).ReturnsAsync
+            (
+                new UserInfo
+                {
+                    UserId = 0,
+                    Name = "test",
+                    Email = "test@aiape.icu"
+                }
+            );
+            UserController controller = new UserController(mockServ.Object, mockRepo.Object, mockTMO.Object);
+
+            var ret = await controller.GetInternalInfoAsync();
+            var okRet = Assert.IsType<OkObjectResult>(ret);
+            var resRet = okRet.Value;
+            Assert.Equal("success", resRet.GetType().GetProperty("Status").GetValue(resRet, null));
+            Assert.Equal("test@aiape.icu", resRet.GetType().GetProperty("Email").GetValue(resRet, null));
+        }
+
+        [Fact]
+        public async Task GetInternalInfoAsync_Fail()
+        {
+            mockServ.Setup(serv => serv.GetUidFromParameters(It.IsAny<HttpRequest>()))
+            .Returns(0);
+            UserInfo userInfo = null;
+            mockRepo.Setup(repo => repo.SelectUserByIdAsync(0)).ReturnsAsync(userInfo);
+            UserController controller = new UserController(mockServ.Object, mockRepo.Object, mockTMO.Object);
+
+            var ret = await controller.GetInternalInfoAsync();
+            var okRet = Assert.IsType<NotFoundObjectResult>(ret);
+            var resRet = okRet.Value;
+            Assert.Equal("userNotExist", resRet.GetType().GetProperty("Status").GetValue(resRet, null));
+            Assert.Equal("N@N", resRet.GetType().GetProperty("Email").GetValue(resRet, null));
+        }
+
+        [Fact]
+        public async Task GetFullInfoAsync_Unauthorized()
+        {
+            mockServ.Setup(serv => serv.GetUidFromParameters(It.IsAny<HttpRequest>()))
+            .Returns(0);
+            mockServ.Setup(serv => serv.GetUidFromToken(It.IsAny<HttpRequest>()))
+            .Returns(1);
+            mockServ.Setup(serv => serv.GetAuthLevelFromToken(It.IsAny<HttpRequest>()))
+            .Returns(AuthLevel.User);
+            UserController controller = new UserController(mockServ.Object, mockRepo.Object, mockTMO.Object);
+
+            var ret = await controller.GetFullInfoAsync();
+            var okRet = Assert.IsType<UnauthorizedObjectResult>(ret);
+            var resRet = okRet.Value;
+            Assert.Equal("userNotExist", resRet.GetType().GetProperty("Status").GetValue(resRet, null));
+            Assert.Equal(-1, resRet.GetType().GetProperty("Uid").GetValue(resRet, null));
+        }
+
+        [Fact]
+        public async Task GetFullInfoAsync_NotFound()
+        {
+            mockServ.Setup(serv => serv.GetUidFromParameters(It.IsAny<HttpRequest>()))
+            .Returns(0);
+            mockServ.Setup(serv => serv.GetUidFromToken(It.IsAny<HttpRequest>()))
+            .Returns(0);
+            UserInfo userInfo = null;
+            mockRepo.Setup(repo => repo.SelectUserByIdAsync(0)).ReturnsAsync(userInfo);
+            UserController controller = new UserController(mockServ.Object, mockRepo.Object, mockTMO.Object);
+
+            var ret = await controller.GetFullInfoAsync();
+            var okRet = Assert.IsType<NotFoundObjectResult>(ret);
+            var resRet = okRet.Value;
+            Assert.Equal("userNotExist", resRet.GetType().GetProperty("Status").GetValue(resRet, null));
+            Assert.Equal(0, resRet.GetType().GetProperty("Uid").GetValue(resRet, null));
+            Assert.Equal("", resRet.GetType().GetProperty("Name").GetValue(resRet, null));
+        
+        }
+
+        [Fact]
+        public async Task GetFullInfoAsync_Ok()
+        {
+            mockServ.Setup(serv => serv.GetUidFromParameters(It.IsAny<HttpRequest>()))
+            .Returns(0);
+            mockServ.Setup(serv => serv.GetUidFromToken(It.IsAny<HttpRequest>()))
+            .Returns(0);
+            mockRepo.Setup(repo => repo.SelectUserByIdAsync(0)).ReturnsAsync
+            (
+                new UserInfo
+                {
+                    UserId = 1,
+                    Name = "test",
+                    Email = "test@aiape.icu",
+                    Auth = AuthLevel.Admin
+                }
+            );
+            UserController controller = new UserController(mockServ.Object, mockRepo.Object, mockTMO.Object);
+
+            var ret = await controller.GetFullInfoAsync();
+            var okRet = Assert.IsType<OkObjectResult>(ret);
+            var resRet = okRet.Value;
+            Assert.Equal("success", resRet.GetType().GetProperty("Status").GetValue(resRet, null));
+            Assert.Equal(1, resRet.GetType().GetProperty("Uid").GetValue(resRet, null));
+            Assert.Equal(2, resRet.GetType().GetProperty("Auth").GetValue(resRet, null));
+        }
+
+        [Fact]
+        public async Task ModifyAsync_LackOfAuth()
+        {
+            body.Uid = 2;
+            mockServ.Setup(serv => serv.GetUidFromToken(It.IsAny<HttpRequest>()))
+            .Returns(1);
+            mockServ.Setup(serv => serv.GetAuthLevelFromToken(It.IsAny<HttpRequest>()))
+            .Returns(AuthLevel.User);
+            UserController controller = new UserController(mockServ.Object, mockRepo.Object, mockTMO.Object);
+
+            var ret = await controller.ModifyAsync(body);
+            var okRet = Assert.IsType<UnauthorizedObjectResult>(ret);
+            var resRet = okRet.Value;
+            Assert.Equal("fail", resRet.GetType().GetProperty("Status").GetValue(resRet, null));
+            Assert.Equal("lack of authority", resRet.GetType().GetProperty("Message").GetValue(resRet, null));
+        }
+
+        [Fact]
+        public async Task ModifyAsync_UserNotExist()
+        {
+            body.Uid = 1;
+            mockServ.Setup(serv => serv.GetUidFromToken(It.IsAny<HttpRequest>()))
+            .Returns(1);
+            UserInfo userInfo = null;
+            mockRepo.Setup(repo => repo.SelectUserByIdAsync(1)).ReturnsAsync(userInfo);
+            UserController controller = new UserController(mockServ.Object, mockRepo.Object, mockTMO.Object);
+
+            var ret = await controller.ModifyAsync(body);
+            var okRet = Assert.IsType<ConflictObjectResult>(ret);
+            var resRet = okRet.Value;
+            Assert.Equal("userNotExist", resRet.GetType().GetProperty("Status").GetValue(resRet, null));
+            Assert.Equal("user dose not exist", resRet.GetType().GetProperty("Message").GetValue(resRet, null));
+        }
+
+        [Fact]
+        public async Task ModifyAsync_NameInvalid()
+        {
+            body.Uid = 1;
+            body.Name = "!^*#";
+            mockServ.Setup(serv => serv.GetUidFromToken(It.IsAny<HttpRequest>()))
+            .Returns(1);
+            mockRepo.Setup(repo => repo.SelectUserByIdAsync(1)).ReturnsAsync(new UserInfo());
+            mockServ.Setup(serv => serv.CheckName("!^*#"))
+            .Returns(false);
+            UserController controller = new UserController(mockServ.Object, mockRepo.Object, mockTMO.Object);
+
+            var ret = await controller.ModifyAsync(body);
+            var okRet = Assert.IsType<ConflictObjectResult>(ret);
+            var resRet = okRet.Value;
+            Assert.Equal("nameInvalid", resRet.GetType().GetProperty("Status").GetValue(resRet, null));
+            Assert.Equal("either contains illegal characters or length of name is too long or too short", resRet.GetType().GetProperty("Message").GetValue(resRet, null));
+        }
+
+        [Fact]
+        public async Task ModifyAsync_PasswordInvalid()
+        {
+            body.Uid = 1;
+            body.Password = "!";
+            mockServ.Setup(serv => serv.GetUidFromToken(It.IsAny<HttpRequest>()))
+            .Returns(1);
+            mockRepo.Setup(repo => repo.SelectUserByIdAsync(1)).ReturnsAsync(new UserInfo());
+            mockServ.Setup(serv => serv.CheckPassword("!"))
+            .Returns(false);
+            UserController controller = new UserController(mockServ.Object, mockRepo.Object, mockTMO.Object);
+
+            var ret = await controller.ModifyAsync(body);
+            var okRet = Assert.IsType<ConflictObjectResult>(ret);
+            var resRet = okRet.Value;
+            Assert.Equal("passwordInvalid", resRet.GetType().GetProperty("Status").GetValue(resRet, null));
+            Assert.Equal("either contains illegal characters or length of password is too long or too short", resRet.GetType().GetProperty("Message").GetValue(resRet, null));
+        
+        }
+
+        [Fact]
+        public async Task ModifyAsync_PasswordSame()
+        {
+            body.Uid = 1;
+            body.Password = "!";
+            mockServ.Setup(serv => serv.GetUidFromToken(It.IsAny<HttpRequest>()))
+            .Returns(1);
+            mockRepo.Setup(repo => repo.SelectUserByIdAsync(1)).ReturnsAsync(new UserInfo
+            {
+                Bcrypt = BNBCrypt.HashPassword(body.Password)
+            });
+            mockServ.Setup(serv => serv.CheckPassword("!"))
+            .Returns(true);
+            UserController controller = new UserController(mockServ.Object, mockRepo.Object, mockTMO.Object);
+
+            var ret = await controller.ModifyAsync(body);
+            var okRet = Assert.IsType<ConflictObjectResult>(ret);
+            var resRet = okRet.Value;
+            Assert.Equal("passwordSame", resRet.GetType().GetProperty("Status").GetValue(resRet, null));
+            Assert.Equal("new password can not be the same as original one", resRet.GetType().GetProperty("Message").GetValue(resRet, null));
+        
+        }
+
+        [Fact]
+        public async Task ModifyAsync_LackOfAuth1()
+        {
+            body.Uid = 1;
+            body.Auth = 2;
+            mockServ.Setup(serv => serv.GetUidFromToken(It.IsAny<HttpRequest>()))
+            .Returns(1);
+            mockServ.Setup(serv => serv.GetAuthLevelFromToken(It.IsAny<HttpRequest>()))
+            .Returns(AuthLevel.User);
+            mockRepo.Setup(repo => repo.SelectUserByIdAsync(1)).ReturnsAsync(new UserInfo());
+            UserController controller = new UserController(mockServ.Object, mockRepo.Object, mockTMO.Object);
+
+            var ret = await controller.ModifyAsync(body);
+            var okRet = Assert.IsType<UnauthorizedObjectResult>(ret);
+            var resRet = okRet.Value;
+            Assert.Equal("fail", resRet.GetType().GetProperty("Status").GetValue(resRet, null));
+            Assert.Equal("lack of authority", resRet.GetType().GetProperty("Message").GetValue(resRet, null));
+        }
+
+        [Fact]
+        public async Task ModifyAsync_TryChangeSelfAuth()
+        {
+            body.Uid = 1;
+            body.Auth = 2;
+            mockServ.Setup(serv => serv.GetUidFromToken(It.IsAny<HttpRequest>()))
+            .Returns(1);
+            mockServ.Setup(serv => serv.GetAuthLevelFromToken(It.IsAny<HttpRequest>()))
+            .Returns(AuthLevel.Admin);
+            mockRepo.Setup(repo => repo.SelectUserByIdAsync(1)).ReturnsAsync(new UserInfo());
+            UserController controller = new UserController(mockServ.Object, mockRepo.Object, mockTMO.Object);
+
+            var ret = await controller.ModifyAsync(body);
+            var okRet = Assert.IsType<ConflictObjectResult>(ret);
+            var resRet = okRet.Value;
+            Assert.Equal("tryChangeSelfAuth", resRet.GetType().GetProperty("Status").GetValue(resRet, null));
+            Assert.Equal("try to change authority of current user", resRet.GetType().GetProperty("Message").GetValue(resRet, null));
+        }
+
+        [Fact]
+        public async Task ModifyAsync_NameExisted()
+        {
+            body.Uid = 1;
+            mockServ.Setup(serv => serv.GetUidFromToken(It.IsAny<HttpRequest>()))
+            .Returns(1);
+            mockRepo.Setup(repo => repo.SelectUserByIdAsync(1)).ReturnsAsync(new UserInfo());
+            mockRepo.Setup(repo => repo.UpdateUserAsync(It.IsAny<UserInfo>()))
+            .ThrowsAsync(new NameHasExistException(""));
+            UserController controller = new UserController(mockServ.Object, mockRepo.Object, mockTMO.Object);
+
+            var ret = await controller.ModifyAsync(body);
+            var okRet = Assert.IsType<ConflictObjectResult>(ret);
+            var resRet = okRet.Value;
+            Assert.Equal("NameExisted", resRet.GetType().GetProperty("Status").GetValue(resRet, null));
+            Assert.Equal("name has been used by other user", resRet.GetType().GetProperty("Message").GetValue(resRet, null));
+        }
+
+        [Fact]
+        public async Task ModifyAsync_Success()
+        {
+            body.Uid = 1;
+            mockServ.Setup(serv => serv.GetUidFromToken(It.IsAny<HttpRequest>()))
+            .Returns(1);
+            mockRepo.Setup(repo => repo.SelectUserByIdAsync(1)).ReturnsAsync(new UserInfo());
+            mockRepo.Setup(repo => repo.UpdateUserAsync(It.IsAny<UserInfo>()));
+            UserController controller = new UserController(mockServ.Object, mockRepo.Object, mockTMO.Object);
+
+            var ret = await controller.ModifyAsync(body);
+            var okRet = Assert.IsType<OkObjectResult>(ret);
+            var resRet = okRet.Value;
+            Assert.Equal("success", resRet.GetType().GetProperty("Status").GetValue(resRet, null));
+            Assert.Equal("modify successfully", resRet.GetType().GetProperty("Message").GetValue(resRet, null));
+        
+        }
+
+        [Fact]
+        public async Task QuestionsAsync_NotFound()
+        {
+            mockServ.Setup(serv => serv.GetUidFromParameters(It.IsAny<HttpRequest>()))
+            .Returns(1);
+            IEnumerable<int> qids = null;
+            mockRepo.Setup(repo => repo.SelectQuestionsIdByIdOrderByModifyTimeAsync(1)).ReturnsAsync(qids);
+            UserController controller = new UserController(mockServ.Object, mockRepo.Object, mockTMO.Object);
+
+            var ret = await controller.QuestionsAsync();
+            var okRet = Assert.IsType<NotFoundObjectResult>(ret);
+            var resRet = okRet.Value;
+            Assert.Equal("userNotExist", resRet.GetType().GetProperty("Status").GetValue(resRet, null));
+            Assert.Equal("user dose not exist", resRet.GetType().GetProperty("Message").GetValue(resRet, null));
+        }
+
+        [Fact]
+        public async Task QuestionsAsync_Ok()
+        {
+            mockServ.Setup(serv => serv.GetUidFromParameters(It.IsAny<HttpRequest>()))
+            .Returns(1);
+            mockRepo.Setup(repo => repo.SelectQuestionsIdByIdOrderByModifyTimeAsync(1)).ReturnsAsync(new int[0]);
+            UserController controller = new UserController(mockServ.Object, mockRepo.Object, mockTMO.Object);
+
+            var ret = await controller.QuestionsAsync();
+            var okRet = Assert.IsType<OkObjectResult>(ret);
+            var resRet = okRet.Value;
+            Assert.Equal("success", resRet.GetType().GetProperty("Status").GetValue(resRet, null));
+            Assert.Equal("get questions for user1 successfully", resRet.GetType().GetProperty("Message").GetValue(resRet, null));
+        }
+
+        [Fact]
+        public async Task AnswersAsync_NotFound()
+        {
+            mockServ.Setup(serv => serv.GetUidFromParameters(It.IsAny<HttpRequest>()))
+            .Returns(1);
+            IEnumerable<int> aids = null;
+            mockRepo.Setup(repo => repo.SelectAnswersIdByIdByModifyTimeAsync(1)).ReturnsAsync(aids);
+            UserController controller = new UserController(mockServ.Object, mockRepo.Object, mockTMO.Object);
+
+            var ret = await controller.AnswersAsync();
+            var okRet = Assert.IsType<NotFoundObjectResult>(ret);
+            var resRet = okRet.Value;
+            Assert.Equal("userNotExist", resRet.GetType().GetProperty("Status").GetValue(resRet, null));
+            Assert.Equal("user dose not exist", resRet.GetType().GetProperty("Message").GetValue(resRet, null));
+        }
+
+        [Fact]
+        public async Task AnswersAsync_Ok()
+        {
+            mockServ.Setup(serv => serv.GetUidFromParameters(It.IsAny<HttpRequest>()))
+            .Returns(1);
+            mockRepo.Setup(repo => repo.SelectAnswersIdByIdByModifyTimeAsync(1)).ReturnsAsync(new int[0]);
+            UserController controller = new UserController(mockServ.Object, mockRepo.Object, mockTMO.Object);
+
+            var ret = await controller.AnswersAsync();
+            var okRet = Assert.IsType<OkObjectResult>(ret);
+            var resRet = okRet.Value;
+            Assert.Equal("success", resRet.GetType().GetProperty("Status").GetValue(resRet, null));
+            Assert.Equal("get answers for user1 successfully", resRet.GetType().GetProperty("Message").GetValue(resRet, null));
+        
+        }
+
+        [Fact]
         public async Task Fresh_Async_TokenInvalid()
         {
             mockServ.Setup(serv => serv.GetExpirationFromToken(It.IsAny<UserBody>())).Throws(new ArgumentException());
@@ -147,6 +488,8 @@ namespace AIBotTest.Controller
             Assert.Equal("fail", resRet.GetType().GetProperty("Status").GetValue(resRet, null));
             Assert.Equal("invalid token", resRet.GetType().GetProperty("Message").GetValue(resRet, null));
         }
+
+
 
         [Fact]
         public async Task FreshAsync_Expired()
@@ -174,6 +517,8 @@ namespace AIBotTest.Controller
             Assert.Equal("fail", resRet.GetType().GetProperty("Status").GetValue(resRet, null));
             Assert.Equal("user has been removed", resRet.GetType().GetProperty("Message").GetValue(resRet, null));
         }
+
+
 
         [Fact]
         public async Task FreshAsync_Success()
