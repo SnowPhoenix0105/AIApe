798babfffb609773908bc3ee8b4baa297d808225
董俊杰 <923861450@qq.com>
2021-05-31 20:58:18 +0800

    移动端底部条均匀

--- frontend/src/components/Mobile/MobileTopBar.vue
+++ frontend/src/components/Mobile/MobileTopBar.vue
@@ -1,11 +1,17 @@
 <template>
     <el-header height="20%">
+
         <el-avatar src="https://cube.elemecdn.com/0/88/03b0d39583f48206768a7534e55bcpng.png"
                    size="large" @click.native="login"></el-avatar>
-        <i class="el-icon-chat-dot-round" :class="(this.index === 0)? 'selected' : 'unselected'" @click="showChat"></i>
-        <i class="el-icon-edit" :class="(this.index === 1)? 'selected' : 'unselected'" @click="showRaiseQuestion"></i>
+        <i class="el-icon-chat-dot-round" :class="(this.index === 0)? 'selected' : 'unselected'"
+           @click="showChat"></i>
+
+        <i class="el-icon-edit" :class="(this.index === 1)? 'selected' : 'unselected'"
+           @click="showRaiseQuestion"></i>
+
         <i class="el-icon-notebook-2" :class="(this.index === 2)? 'selected' : 'unselected'"
            @click="showQuestionList"></i>
+
         <i class="el-icon-paperclip" :class="(this.index === 3)? 'selected' : 'unselected'"
            @click="showQuestionDetail"></i>
     </el-header>
@@ -51,15 +57,16 @@ export default {
 }
 
 .el-avatar {
+    margin-left: 2vw;
     margin-top: 2%;
     margin-bottom: 1%;
     cursor: pointer;
 }
 
 i {
-    margin-left: 10%;
+    margin-left: 12vw;
     margin-right: 0;
-    font-size: 150%;
+    font-size: 8vw;
     cursor: pointer;
 }
 
