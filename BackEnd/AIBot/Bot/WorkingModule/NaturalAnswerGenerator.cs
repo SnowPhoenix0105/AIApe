95ffc6daf59affd3bacccd22a61f0b6ae8533bef
SnowPhoenix <1595763343@qq.com>
2021-06-15 21:09:54 +0800

    finish weather

--- BackEnd/AIBot/Bot/WorkingModule/NaturalAnswerGenerator.cs
+++ BackEnd/AIBot/Bot/WorkingModule/NaturalAnswerGenerator.cs
@@ -8,6 +8,12 @@ using Microsoft.EntityFrameworkCore;
 using Buaa.AIBot.Services;
 using Microsoft.Extensions.Logging;
 using Buaa.AIBot.Bot.Framework;
+using System.Net;
+using System.IO.Compression;
+using System.IO;
+using System.Diagnostics;
+using System.Text.RegularExpressions;
+using System.Text;
 
 namespace Buaa.AIBot.Bot.WorkingModule
 {
@@ -17,6 +23,7 @@ namespace Buaa.AIBot.Bot.WorkingModule
         private INLPService nlpService;
         private CancellationToken cancellationToken;
         private ILogger<NaturalAnswerGenerator> logger;
+        private Regex weatherRegex = new Regex("<input type=\"hidden\" id=\"hidden_title\" value=\"(?<weather>[^>\"]+)\" />");
 
         public NaturalAnswerGenerator(DatabaseContext context, INLPService nlpService, Utils.GlobalCancellationTokenSource gcts, ILogger<NaturalAnswerGenerator> logger)
         {
@@ -68,11 +75,85 @@ namespace Buaa.AIBot.Bot.WorkingModule
             return $"现在是北京时间{date.Year}年{date.Month}月{date.Day}日{date.Hour}时{date.Minute}分{date.Second}秒哟{Kaomojis.Happy}";
         }
 
+        private async Task<string> GetHtmlForWeatherAsync()
+        {
+            string url = "http://www.weather.com.cn/weather1d/101010100.shtml";
+            var request = WebRequest.CreateHttp(url);
+            {
+                request.Accept = "*/*";
+                request.ServicePoint.Expect100Continue = false;
+                request.ServicePoint.UseNagleAlgorithm = false;
+                request.AllowWriteStreamBuffering = false;
+                request.Headers.Add(HttpRequestHeader.AcceptEncoding, "gzip,deflate");
+                request.ContentType = "application/x-www-form-urlencoded";
+                request.AllowAutoRedirect = false;
+                request.UserAgent = "Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/50.0.2661.102 Safari/537.36";
+                request.Timeout = 5000;
+                request.KeepAlive = false;
+                request.Method = "GET";
+            }
+
+            string ret = null;
+            var response = (HttpWebResponse)await request.GetResponseAsync();
+            {
+                var stream = response.GetResponseStream();
+                if (response.ContentEncoding != null)
+                {
+                    if (response.ContentEncoding.ToLower().Contains("gzip"))
+                    {
+                        using (GZipStream gzip = new GZipStream(stream, CompressionMode.Decompress))
+                        {
+                            using (StreamReader reader = new StreamReader(gzip, Encoding.UTF8))
+                            {
+                                ret = reader.ReadToEnd();
+                            }
+                        }
+                    }
+                    else if (response.ContentEncoding.ToLower().Contains("deflate"))
+                    {
+                        using (DeflateStream deflate = new DeflateStream(stream, CompressionMode.Decompress))
+                        {
+                            using (StreamReader reader = new StreamReader(deflate, Encoding.UTF8))
+                            {
+                                ret = reader.ReadToEnd();
+                            }
+                        }
+                    }
+                }
+                if (ret == null)
+                {
+                    using (StreamReader reader = new StreamReader(stream, Encoding.UTF8))
+                    {
+
+                        ret = reader.ReadToEnd();
+                    }
+                }
+            }
+            return ret;
+        }
+
+        private async Task<string> GenerateWeatherAsync()
+        {
+            string html = await GetHtmlForWeatherAsync();
+            logger.LogInformation("html={html}", html);
+            var m = weatherRegex.Match(html);
+            if (!m.Success)
+            {
+                return "小猿不太清楚呢，您可以到[url http://www.weather.com.cn]进行查看";
+            }
+            var info = m.Groups["weather"];
+            return $"小猿在北京这边的天气情况是这样的：{info}，您可以到[url http://www.weather.com.cn]查看更多信息哟";
+        }
+
         private async Task<string> FinalProduceAsync(string origin)
         {
             if (origin == "[call date]")
             {
                 return GenerateDate();
+            }
+            if (origin == "[call weather]")
+            {
+                return await GenerateWeatherAsync();
             }
             origin = origin
                 .Replace("[Happy]", Kaomojis.Happy)
